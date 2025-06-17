534e3a732477926299bb63135c305fdbcea77601
Puja0208 <pooja2081997@gmail.com>
2025-06-17 15:34:50 +0530

    feat: Explore get , post, delete calls

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ const app = express();
 const port = 3000;
 
 /**Request handler */
-app.use("/test",(req, res) => {
-  res.send("hello");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "john", lastName: "Doe" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("save data to db");
+  res.send("data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("delete successfully");
 });
 
 app.listen(port, () => {
