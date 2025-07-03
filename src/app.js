2d7822934e9b497c68b73a1742e72933ce04235f
Puja0208 <pooja2081997@gmail.com>
2025-07-03 23:06:32 +0530

    feat:validate req body and encypt passowrd

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 //create a new express js application
 const app = express();
@@ -19,12 +20,44 @@ connectDB()
   .catch((err) => console.log(err));
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
   try {
+    //Validate data
+    validateSignUpData(req);
+
+    const { password, firstName, lastName, emailId } = req.body;
+
+    //encrypt password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.send("user added successfully");
   } catch (error) {
     res.status(400).send(`error saving user:${error}`);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("email id not present in db");
+    }
+
+    const isPasswodValid = await bcrypt.compare(password, user.password);
+    if (isPasswodValid) {
+      res.send("Login success");
+    } else {
+      throw new Error("password is not valid");
+    }
+  } catch (error) {
+    res.status(400).send(`error in login ${error}`);
   }
 });
 
