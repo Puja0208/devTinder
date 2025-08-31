c2bdc9d065b63ed98005abe942bb34a50fb068fe
Puja0208 <pooja2081997@gmail.com>
2025-08-31 08:04:50 +0530

    fix: Create get all connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,11 +13,39 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId",["firstName","lastName"]);
+    }).populate("fromUserId", ["firstName", "lastName"]); // can write seoncind param as as 'firstName lastName'
 
     return res.json({
       message: "all request",
       connectionRequests,
+    });
+  } catch (error) {
+    res.status(400).send(`Error: ${error.message}`);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    //Puja -> Joy - accpeted
+    //Joy -> Sam - accpeted
+    //mesn check if logged in user is from user or to user as he can accept/send requests (which gets accpeted)
+    const allConnections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        {
+          fromUserId: loggedInUser._id,
+          status: "accepted",
+        },
+      ],
+    }).populate("fromUserId", ["firstName", "lastName"]);
+
+    //filter data to only sen fro details not cpnnection details
+    const data = allConnections.map((row) => row.fromUserId);
+    res.json({
+      message: "connections fetched",
+      data,
     });
   } catch (error) {
     res.status(400).send(`Error: ${error.message}`);
