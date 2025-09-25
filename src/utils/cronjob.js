9ef3d1f57d95ef91286a05893a53bd26cd4ac26e
Puja0208 <pooja2081997@gmail.com>
2025-09-25 10:23:13 +0530

    feat: Add cron job to send emails

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,8 +1,40 @@
 const cron = require("node-cron");
-const {subDays} = require("date-fns")
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+// const sendEmail = require("./send");
 const ConnectionRequest = require("../models/connectionRequest");
-cron.schedule("0 * * * *", () => {
-    //send email to all people who got request the previous day
-  
-    console.log("hello World, " + new Date());
-})
+cron.schedule("0 * * * *", async () => {
+  //send email to all people who got request the previous day
+  try {
+    const yesterday = subDays(new Date(), 1);
+    const yesterayStart = startOfDay(yesterday);
+    const yesterdayEnd = endOfDay(yesterday);
+    const pendingRequests = await ConnectionRequest.find({
+      status: "interested",
+      createdAt: {
+        $gte: yesterayStart,
+        $lt: yesterdayEnd,
+      },
+    }).populate("fromUserId toUserId");
+
+    //create list of all toUserId emails - use set have all unique email ids
+    const listOfEmails = [
+      new Set(pendingRequests.map((req) => req.toUserId.emailId)),
+    ];
+
+    /*
+    for (const email of listOfEmails) {
+      //send emails
+      try {
+        const res = await sendEmail.run(
+          "New friend request pending for" + toEmailid,
+          "There are so may request opending.Please login to accept/reject it"
+        );
+        console.log(res);
+      } catch (error) {}
+    }
+      */
+  } catch (error) {
+    console.error(error);
+  }
+  console.log("hello World, " + new Date());
+});
