e78768ee787958608f7c1011ff9215b40ca867c8
Puja0208 <pooja2081997@gmail.com>
2025-09-30 06:41:01 +0530

    feat: Initialize socket

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const connectDB = require("./config/database");
 var cors = require("cors");
 
-require("./utils/cronjob")
+const http = require("http");
+
+require("./utils/cronjob");
 
 const cookieParser = require("cookie-parser");
 const User = require("./models/user");
-
 
 //create a new express js application
 const app = express();
@@ -26,18 +27,23 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
-const paymentRouter = require("./routes/payment")
+const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-app.use("/", paymentRouter)
+app.use("/", paymentRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
+
 connectDB()
   .then(async () => {
     console.log("db connect success");
     await User.syncIndexes();
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
   })
