d4ada3fba736c74e34190b7c8c7ea6ee74dd3285
Puja0208 <pooja2081997@gmail.com>
2025-09-30 07:10:35 +0530

    feat: Create chat room

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -7,7 +7,11 @@ const initializeSocket = (server) => {
   });
   io.on("connection", (socket) => {
     //handle events
-    socket.on("joinChat", () => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      console.log(`${firstName} joined room ${roomId}`);
+      socket.join(roomId);
+    });
 
     socket.on("sendMessage", () => {});
 
